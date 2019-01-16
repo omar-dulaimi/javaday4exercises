c9d7eb12cc69d69de43c4e1a0c750c864c8e25e8
omar-dulaimi <o.m.dulaimi@gmail.com>
2019-01-16 14:55:19 +0200

    fix typo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	//Foo objFoo = new Foo();  //abstarct classes can't be instantiated
+	//Foo objFoo = new Foo();  //abstract classes can't be instantiated
 	Boo objBoo = new Boo();
     }
 }
